d556828c5d695a03f08f618404b485fdf7e117f5
Larisa <larisamash01@gmail.com>
2022-09-25 02:58:51 +0300

    1 задача домашки

---
+++ homework1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine(); // строка
+void checking(string number){ // проверка
+
+  if (number[0]==number[4] && number[1]==number[3]){
+    Console.WriteLine($"{number} -> да");
+  }
+  else Console.WriteLine($"{number} -> нет");
+}
+
+if (number!.Length == 5){
+  checking(number);
+}
+else 
+Console.WriteLine($"Ошибка. Введено не пятизначное число");
