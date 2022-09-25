da842a761848b71194b3f6d164662f44ce2138bb
Larisa <larisamash01@gmail.com>
2022-09-25 03:20:21 +0300

    3 задача домашки

---
+++ homework3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 1; // ввести счетчик
+
+Console.Write($"{number} -> ");
+while (count < number)
+{
+    Console.Write($"{Math.Pow((count), 3)}, ");
+    count++;
+}
+if (count == number) // чтобы не было в конце запятой
+{
+    Console.Write($"{Math.Pow((count), 3)}");
+}
